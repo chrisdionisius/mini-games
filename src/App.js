e9fe2795323e8bbcb640d55fe1af3e992b119cd8
chrisdionisius <chrisdionisius@gmail.com>
2023-05-11 16:07:00 +0700

    finished using Spinner as class component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useRef } from "react";
+import Spinner from "./Components/Spinner";
+import RepeatButton from "./Components/RepeatButton";
+import WinningSound from "./Components/WinningSound";
+import "./App.css";
+const loser = [
+  "Not quite",
+  "Stop gambling",
+  "Hey, you lost!",
+  "Ouch! I felt that",
+  "Don't beat yourself up",
+  "There goes the college fund",
+  "I have a cat. You have a loss",
+  "You're awesome at losing",
+  "Coding is hard",
+  "Don't hate the coder",
+];
 function App() {
+  const [winner, setWinner] = useState(null);
+  const child1Ref = useRef();
+  const child2Ref = useRef();
+  const child3Ref = useRef();
+
+  const handleClick = () => {
+    setWinner(null);
+    emptyArray();
+    child1Ref.current.reset();
+    child2Ref.current.reset();
+    child3Ref.current.reset();
+  };
+
+  let matches = [];
+
+  const finishHandler = (value) => {
+    matches.push(value);
+
+    if (matches.length === 3) {
+      const first = matches[0];
+      const results = matches.every((match) => match === first);
+      setWinner(results);
+    }
+  };
+
+  const emptyArray = () => {
+    matches = [];
+  };
+
+  const getLoser = () => {
+    return loser[Math.floor(Math.random() * loser.length)];
+  };
+
+  let repeatButton = null;
+  let winningSound = null;
+
+  if (winner !== null) {
+    repeatButton = <RepeatButton onClick={handleClick} />;
+  }
+
+  if (winner) {
+    winningSound = <WinningSound />;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {winningSound}
+      <h1 style={{ color: "white" }}>
+        <span>
+          {winner === null
+            ? "Waiting…"
+            : winner
+            ? "🤑 Pure skill! 🤑"
+            : getLoser()}
+        </span>
+      </h1>
+
+      <div className={`spinner-container`}>
+        <Spinner onFinish={finishHandler} ref={child1Ref} timer="1000" />
+        <Spinner onFinish={finishHandler} ref={child2Ref} timer="1400" />
+        <Spinner onFinish={finishHandler} ref={child3Ref} timer="2200" />
+        <div className="gradient-fade"></div>
+      </div>
+      {repeatButton}
     </div>
   );
 }
---
+++ src/Components/Spinner.js
@@ -0,0 +1,124 @@
+import React from "react";
+
+class Spinner extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // Bind `forceUpdateHandler` to this instance of `Spinner`
+    this.forceUpdateHandler = this.forceUpdateHandler.bind(this);
+  }
+
+  // Define the `forceUpdateHandler` function
+  forceUpdateHandler() {
+    // Call the `reset` function to reset the spinner
+    this.reset();
+  }
+
+  // Define the initial state of the spinner
+  state = {
+    position: 0,
+    lastPosition: null,
+  };
+
+  // Define the height of each icon
+  static iconHeight = 188;
+
+  // Generate a random speed multiplier between 1 and 3
+  multiplier = Math.floor(Math.random() * (4 - 1) + 1);
+
+  // Calculate the starting position of the spinner
+  start = this.setStartPosition();
+
+  // Calculate the speed of the spinner based on the multiplier and icon height
+  speed = Spinner.iconHeight * this.multiplier;
+
+  // Calculate a random starting position for the spinner
+  setStartPosition() {
+    return Math.floor(Math.random() * 9) * Spinner.iconHeight * -1;
+  }
+
+  // Move the background of the spinner to create the spinning effect
+  moveBackground() {
+    this.setState({
+      position: this.state.position - this.speed,
+      timeRemaining: this.state.timeRemaining - 100,
+    });
+  }
+
+  // Get the symbol from the current position of the spinner
+  getSymbolFromPosition() {
+    let { position } = this.state;
+    const totalSymbols = 9;
+    const maxPosition = Spinner.iconHeight * (totalSymbols - 1) * -1;
+    let moved = (this.props.timer / 100) * this.multiplier;
+    let startPosition = this.start;
+    let currentPosition = startPosition;
+
+    for (let i = 0; i < moved; i++) {
+      currentPosition -= Spinner.iconHeight;
+
+      if (currentPosition < maxPosition) {
+        currentPosition = 0;
+      }
+    }
+
+    // Call the `onFinish` function with the current position of the spinner
+    this.props.onFinish(currentPosition);
+  }
+
+  // Update the spinner every 100 milliseconds
+  tick() {
+    if (this.state.timeRemaining <= 0) {
+      clearInterval(this.timer);
+      this.getSymbolFromPosition();
+    } else {
+      this.moveBackground();
+    }
+  }
+
+  // When the component mounts, set the initial state of the spinner and start the timer
+  componentDidMount() {
+    clearInterval(this.timer);
+
+    this.setState({
+      position: this.start,
+      timeRemaining: this.props.timer,
+    });
+
+    this.timer = setInterval(() => {
+      this.tick();
+    }, 100);
+  }
+
+  // Reset the spinner to its initial state
+  reset() {
+    if (this.timer) {
+      clearInterval(this.timer);
+    }
+
+    this.start = this.setStartPosition();
+
+    this.setState({
+      position: this.start,
+      timeRemaining: this.props.timer,
+    });
+
+    this.timer = setInterval(() => {
+      this.tick();
+    }, 100);
+  }
+
+  // Render the spinner with its current position
+  render() {
+    let { position, current } = this.state;
+
+    return (
+      <div
+        style={{ backgroundPosition: "0px " + position + "px" }}
+        className={`icons`}
+      />
+    );
+  }
+}
+
+export default Spinner;
