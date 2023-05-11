f7e353894a790ccdd75a4519ed4a125f255804e1
chrisdionisius <chrisdionisius@gmail.com>
2023-05-11 17:54:56 +0700

    convert Spinner component into functional component

--- src/Components/Spinner.js
+++ src/Components/Spinner.js
@@ -1,60 +1,60 @@
-import React from "react";
+import React, {
+  useEffect,
+  useState,
+  forwardRef,
+  useCallback,
+  useImperativeHandle,
+} from "react";
 
-class Spinner extends React.Component {
-  constructor(props) {
-    super(props);
-
-    // Bind `forceUpdateHandler` to this instance of `Spinner`
-    this.forceUpdateHandler = this.forceUpdateHandler.bind(this);
-  }
-
-  // Define the `forceUpdateHandler` function
-  forceUpdateHandler() {
-    // Call the `reset` function to reset the spinner
-    this.reset();
-  }
-
-  // Define the initial state of the spinner
-  state = {
+const Spinner = forwardRef((props, ref) => {
+  const [state, setState] = useState({
     position: 0,
     lastPosition: null,
-  };
+    timeRemaining: props.timer,
+  });
 
+  useImperativeHandle(ref, () => ({
+    reset() {
+      setState({
+        position: start(),
+        lastPosition: null,
+        timeRemaining: props.timer,
+      });
+    },
+  }));
   // Define the height of each icon
-  iconHeight = 188;
+  const iconHeight = 188;
 
   // Generate a random speed multiplier between 1 and 3
-  multiplier = Math.floor(Math.random() * (4 - 1) + 1);
+  const multiplier = Math.floor(Math.random() * 3 + 1);
 
   // Calculate the starting position of the spinner
-  start = this.setStartPosition();
+  const start = useCallback(() => {
+    return Math.floor(Math.random() * 9) * iconHeight * -1;
+  }, [iconHeight]);
 
   // Calculate the speed of the spinner based on the multiplier and icon height
-  speed = this.iconHeight * this.multiplier;
-
-  // Calculate a random starting position for the spinner
-  setStartPosition() {
-    return Math.floor(Math.random() * 9) * this.iconHeight * -1;
-  }
+  const speed = iconHeight * multiplier;
 
   // Move the background of the spinner to create the spinning effect
-  moveBackground() {
-    this.setState({
-      position: this.state.position - this.speed,
-      timeRemaining: this.state.timeRemaining - 100,
-    });
-  }
+  const moveBackground = useCallback(() => {
+    setState((prevState) => ({
+      ...prevState,
+      position: prevState.position - speed,
+      timeRemaining: prevState.timeRemaining - 100,
+    }));
+  }, [speed]);
 
   // Get the symbol from the current position of the spinner
-  getSymbolFromPosition() {
+  const getSymbolFromPosition = useCallback(() => {
     const totalSymbols = 9;
-    const maxPosition = this.iconHeight * (totalSymbols - 1) * -1;
-    let moved = (this.props.timer / 100) * this.multiplier;
-    let startPosition = this.start;
+    const maxPosition = iconHeight * (totalSymbols - 1) * -1;
+    let moved = (props.timer / 100) * multiplier;
+    let startPosition = start();
     let currentPosition = startPosition;
 
     for (let i = 0; i < moved; i++) {
-      currentPosition -= this.iconHeight;
+      currentPosition -= iconHeight;
 
       if (currentPosition < maxPosition) {
         currentPosition = 0;
@@ -62,62 +62,30 @@ class Spinner extends React.Component {
     }
 
     // Call the `onFinish` function with the current position of the spinner
-    this.props.onFinish(currentPosition);
-  }
+    props.onFinish(currentPosition);
+  }, [props, multiplier, iconHeight, start]);
 
   // Update the spinner every 100 milliseconds
-  tick() {
-    if (this.state.timeRemaining <= 0) {
-      clearInterval(this.timer);
-      this.getSymbolFromPosition();
-    } else {
-      this.moveBackground();
-    }
-  }
-
-  // When the component mounts, set the initial state of the spinner and start the timer
-  componentDidMount() {
-    clearInterval(this.timer);
-
-    this.setState({
-      position: this.start,
-      timeRemaining: this.props.timer,
-    });
-
-    this.timer = setInterval(() => {
-      this.tick();
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (state.timeRemaining <= 0) {
+        clearInterval(timer);
+        getSymbolFromPosition();
+      } else {
+        moveBackground();
+      }
     }, 100);
-  }
 
-  // Reset the spinner to its initial state
-  reset() {
-    if (this.timer) {
-      clearInterval(this.timer);
-    }
-
-    this.start = this.setStartPosition();
-
-    this.setState({
-      position: this.start,
-      timeRemaining: this.props.timer,
-    });
-
-    this.timer = setInterval(() => {
-      this.tick();
-    }, 100);
-  }
+    return () => clearInterval(timer);
+  }, [state.timeRemaining, moveBackground, getSymbolFromPosition]);
 
   // Render the spinner with its current position
-  render() {
-    let { position } = this.state;
-
-    return (
-      <div
-        style={{ backgroundPosition: "0px " + position + "px" }}
-        className={`icons`}
-      />
-    );
-  }
-}
+  return (
+    <div
+      style={{ backgroundPosition: "0px " + state.position + "px" }}
+      className={`icons`}
+    />
+  );
+});
 
 export default Spinner;
